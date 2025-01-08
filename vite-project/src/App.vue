7def1b210ab3e871eb2430a30e60466eff08c2f6
Aily Le <aily.le@on-running.com>
2025-01-08 18:15:20 +0800

    初始化vue+ant design vue

---
+++ vite-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+    <Home />
+  </Layout>
+
+</template>
+
+<script setup lang="ts">
+import Layout from './components/Layout.vue';
+import Home from './pages/Home.vue';
+</script>
+
+<style scoped>
+
+</style>
